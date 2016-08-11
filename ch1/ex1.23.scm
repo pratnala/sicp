763314be0ad94b59c711cbe21f3efe9e7ca4654c
Pratyush Nalam <pratyushnalam@outlook.com>
2016-08-11 15:48:24 -0400

    Committing code for ex1.23

--- ch1/ex1.23.scm
+++ ch1/ex1.23.scm
@@ -1,1 +1,23 @@
 #lang racket
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (next test-divisor)))))
+
+(define (square x)
+  (* x x))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (next divisor)
+  (if (= divisor 2)
+      3
+      (+ divisor 2)))
